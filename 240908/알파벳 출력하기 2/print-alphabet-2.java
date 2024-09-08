7c1f3a96e0325890c1f1cee26da4bc71753c7be1
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-08 19:01:04 +0900

    Title: 알파벳 출력하기 2, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240908/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int start = 65;
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            for(int j = n-i; j > 0; j--) {
+                System.out.print((char)(start+cnt) + " ");
+                cnt++;
+                if(cnt > 25) {
+                    cnt = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
