84b1400295cd2bcd1634f87370d720d2e8eea74f
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-09 16:03:22 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240909/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
+++ 240909/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -7,24 +7,14 @@ public class Main {
         int n = sc.nextInt();
 
         int[] numbers = new int[n];
-        int cnt = 0;
         for(int i = 0; i < n; i++) {
             numbers[i] = sc.nextInt();
-
-            if(numbers[i] % 2 == 0) {
-                cnt++;
-            }
         }
 
-        int[] temp = new int[cnt];
-        for(int i = 0; i < n; i++) {
+        for(int i = n-1; i >= 0; i--) {
             if(numbers[i] % 2 == 0) {
-                temp[i] = numbers[i];
+                System.out.print(numbers[i] + " ");
             }
-        }
-
-        for(int i = cnt-1; i >= 0; i--) {
-            System.out.print(temp[i] + " ");
         }
     }
 }
