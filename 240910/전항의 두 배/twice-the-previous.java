0cedbab648dd0f9a000f8610aa77ba5bb5c146f5
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-10 13:13:46 +0900

    Title: 전항의 두 배, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240910/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] numbers = new int[10];
+        numbers[0] = a;
+        numbers[1] = b;
+        System.out.print(numbers[0] + " " + numbers[1] + " ");
+ 
+        for(int i = 2; i < 10; i++) {
+            numbers[i] = numbers[i-1] + 2*numbers[i-2];
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
