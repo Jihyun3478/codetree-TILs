40bdde33a841c6c21b2eb8bfa21ce64ce706ad9c
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-11 16:00:13 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240911/배열 놀이/play-with-array.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int[] question = new int[3];
+        for(int i = 0; i < q; i++) {
+            question[i] = sc.nextInt();
+
+            if(question[i] == 1) {
+                int a = sc.nextInt();
+                question1(numbers, a);
+
+            } else if(question[i] == 2) {
+                int b = sc.nextInt();
+                question2(numbers, b);
+
+            } else if(question[i] == 3) {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+                question3(numbers, s, e);
+            }
+        }
+    }
+
+    public static void question1(int[] numbers, int a) {
+        System.out.println(numbers[a-1]);
+    }
+
+    public static void question2(int[] numbers, int b) {
+        int cnt = 0;
+        int num = 0;
+        int[] index = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == b) {
+                cnt++;
+                index[num++] = i+1;
+            }
+        }
+
+        if(cnt >= 1) {
+            System.out.println(index[0]);
+        } else if(cnt == 0) {
+            System.out.println("0");
+        }
+    }
+
+    public static void question3(int[] numbers, int s, int e) {
+        for(int i = s-1; i < e; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
