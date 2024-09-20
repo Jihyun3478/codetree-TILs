d97c895a68443375920310ef810fc601a9c2c77d
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-20 12:05:53 +0900

    Title: 격자에 점 그리기, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240920/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] numbers = new int[n][n];
+        int r = 0, c = 0;
+
+        for(int i = 0; i < m; i++) {
+            r = sc.nextInt();
+            c = sc.nextInt();
+            numbers[r-1][c-1] = i+1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
