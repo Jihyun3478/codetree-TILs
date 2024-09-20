6776d68ab3a1455dd517ecff9fc9f7718f8920f8
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-20 11:44:54 +0900

    Title: 격자 반대로 채우기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240920/격자 반대로 채우기/grid-anti-fill.java
+++ 240920/격자 반대로 채우기/grid-anti-fill.java
@@ -9,15 +9,29 @@ public class Main {
         int num = 1;
 
         for(int i = n-1; i >= 0; i--) {
-            if(i % 2 == 1) {
-                for(int j = n-1; j >= 0; j--) {
-                    numbers[j][i] = num;
-                    num++;
+            if(n % 2 == 0) {
+                if(i % 2 == 1) {
+                    for(int j = n-1; j >= 0; j--) {
+                        numbers[j][i] = num;
+                        num++;
+                    }
+                } else if(i % 2 == 0) {
+                    for(int j = 0; j < n; j++) {
+                        numbers[j][i] = num;
+                        num++;
+                    }
                 }
-            } else if(i % 2 == 0) {
-                for(int j = 0; j < n; j++) {
-                    numbers[j][i] = num;
-                    num++;
+            } else if(n % 2 == 1) {
+                if(i % 2 == 0) {
+                    for(int j = n-1; j >= 0; j--) {
+                        numbers[j][i] = num;
+                        num++;
+                    }
+                } else if(i % 2 == 1) {
+                    for(int j = 0; j < n; j++) {
+                        numbers[j][i] = num;
+                        num++;
+                    }
                 }
             }
         }
