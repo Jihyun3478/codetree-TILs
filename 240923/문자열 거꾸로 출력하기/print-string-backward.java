8bbb441362a22c475729d9c3059aadd3e66d95e6
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-23 20:14:04 +0900

    Title: 문자열 거꾸로 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/문자열 거꾸로 출력하기/print-string-backward.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 10; i++) {
+            String s = sc.next();
+            if(s.equals("END")) {
+                break;
+            }
+            StringBuffer sb = new StringBuffer(s);
+            System.out.println(sb.reverse().toString());
+        }
+    }
+}
