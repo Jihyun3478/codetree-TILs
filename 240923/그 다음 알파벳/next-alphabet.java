734f45bfd710e157f63910b0dbfa77d397152ff9
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-23 19:14:02 +0900

    Title: 그 다음 알파벳, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240923/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        int convertA = (int)a+1;
+
+        System.out.print((char)convertA);
+    }
+}
