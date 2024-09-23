0507cd3f922aedceb539c302f8766939ecc96d7d
Jihyun3478 <leejihyun3478@gmail.com>
2024-09-23 19:58:01 +0900

    Title: 정수만 추출하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String newS1 = "";
+        String newS2 = "";
+
+        for(int i = 0; i < s1.length(); i++) {
+            if(Character.isDigit(s1.charAt(i))) {
+                newS1 += s1.charAt(i);
+            }
+            else {
+                break;
+            }
+            
+        }
+
+        for(int i = 0; i < s2.length(); i++) {
+            if(Character.isDigit(s2.charAt(i))) {
+                newS2 += s2.charAt(i);
+            }
+            else {
+                break;
+            }
+            
+        }
+        System.out.print(Integer.parseInt(newS1) + Integer.parseInt(newS2));
+    }
+}
